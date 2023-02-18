4f8bdcec4c1786ad5344c402dee21c038148a1d2
shamim26 <shamimreja101@gmail.com>
2023-02-18 13:12:23 +0600

    random color added

--- js/function.js
+++ js/function.js
@@ -1,7 +1,58 @@
+// Random color
+function randomColor() {
+  const color = Math.floor(Math.random() * 255);
+  return color;
+}
+
+function mouseOver(cardId) {
+  document
+    .getElementById(cardId)
+    .addEventListener("mouseenter", function (evt) {
+      const colorChange = evt.target;
+      colorChange.style.backgroundColor =
+        "rgba(" +
+        randomColor() +
+        "," +
+        randomColor() +
+        "," +
+        randomColor() +
+        ")";
+    });
+}
+function mouseOut(cardId) {
+    document.getElementById(cardId).addEventListener('mouseleave',function (evt) {
+        const colorInitial = evt.target;
+        colorInitial.style.backgroundColor = 'initial';
+    })
+}
+
+// get the title
+function getTitle(Id) {
+  const title = document.getElementById(Id).innerText;
+  return title;
+}
+
 // get input value
 function getInputValue(Id) {
   const inputValue = document.getElementById(Id).value;
   return inputValue;
+}
+
+// validation input
+function InputValidate(input1, input2) {
+  if (
+    isNaN(input1) ||
+    isNaN(input2) ||
+    input1 === "" ||
+    input2 === "" ||
+    input1 < 0 ||
+    input2 < 0
+  ) {
+    alert("Please enter a valid input!");
+    return false;
+  } else {
+    return true;
+  }
 }
 
 // add to area calculation
--- js/main.js
+++ js/main.js
@@ -5,13 +5,19 @@ document
   .getElementById("triangle-calculate")
   .addEventListener("click", function () {
     serial += 1;
-    const title = document.getElementById("triangle-title").innerText;
+    const title = getTitle("triangle-title");
     const triangleBaseInput = getInputValue("triangle-basefield");
-
     const triangleHeightInput = getInputValue("triangle-heightfield");
     // sum
     const triangleArea =
       0.5 * parseFloat(triangleBaseInput) * parseFloat(triangleHeightInput);
+
+    // validation
+    if (!InputValidate(triangleBaseInput, triangleHeightInput)) {
+      document.getElementById('triangle-basefield').value = '';
+      document.getElementById('triangle-heightfield').value = '';
+      return;
+    }
 
     // add to table
     addToTable(serial, title, triangleArea);
